6a019520c0a06f0964a17e380c785c54df5a4891
Reza <163760703+rezasdt@users.noreply.github.com>
2024-05-21 08:48:07 +0200

    add MachineItemUI

--- Assets/Scripts/ObjectPlacer.cs
+++ Assets/Scripts/ObjectPlacer.cs
@@ -1,16 +1,18 @@
-using System;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class ObjectPlacer : MonoBehaviour
 {
-    [SerializeField]
-    private InputManager _inputManager;
-    [SerializeField]
-    private GameObject _cellIndicator;
-    [SerializeField]
-    private Grid _grid;
-    [SerializeField]
-    private GameObject _gridShader;
+    public static Machine SelectedMachine = null;
+
+    [SerializeField] private InputManager _inputManager;
+    [SerializeField] private GameObject _cellIndicator;
+    [SerializeField] private Grid _grid;
+    [SerializeField] private GameObject _gridShader;
+
+    [SerializeField] private MachinesDatabase _machinesDatabase;
+    [SerializeField] private GameObject _machineItemUIPrefab;
+    [SerializeField] private RectTransform _uiPanel;
 
     private void OnEnable()
     {
@@ -24,8 +26,43 @@ public class ObjectPlacer : MonoBehaviour
         _inputManager.Discard -= OnDiscard;
     }
 
+    private void Start()
+    {
+        PopulateUIPanel();
+    }
+
+    private void PopulateUIPanel()
+    {
+        if (_machinesDatabase == null || _machineItemUIPrefab == null || _uiPanel == null)
+        {
+            Debug.LogError("MachinesDatabase, MachineItemUIPrefab, or UIPanel references are not set in ObjectPlacer.");
+            return;
+        }
+
+        foreach (Tier tier in _machinesDatabase.tiers)
+        {
+            foreach (Machine machine in tier.machinesUnlocked)
+            {
+                GameObject machineItemUIObj = Instantiate(_machineItemUIPrefab, _uiPanel);
+                MachineItemUI machineItemUI = machineItemUIObj.GetComponent<MachineItemUI>();
+
+                if (machineItemUI != null)
+                {
+                    machineItemUI.machine = machine;
+                }
+                else
+                {
+                    Debug.LogError("MachineItemUI prefab does not have MachineItemUI component attached.");
+                    return;
+                }
+            }
+        }
+    }
+
+
     private void OnDiscard()
     {
+        SelectedMachine = null;
         _cellIndicator.SetActive(false);
         _gridShader.SetActive(false);
     }
