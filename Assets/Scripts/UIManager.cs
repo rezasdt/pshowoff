d975c6a1847769111696ba4c50368dcdfeca2e5c
Reza <163760703+rezasdt@users.noreply.github.com>
2024-05-22 04:24:32 +0200

    remove Risk UI

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,20 +5,10 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI moneyText;
     [SerializeField] private TextMeshProUGUI daysLeftText;
-    [SerializeField] private TextMeshProUGUI riskTakenText;
 
     private void FixedUpdate()
     {
         moneyText.text = $"Money: ${GameManager.Instance.Money}";
         daysLeftText.text = $"Days Left: {GameManager.Instance.TotalDays}";
-
-        if (GameManager.Instance.RiskTotal > 0)
-        {
-            float riskPercentage = (float)GameManager.Instance.RiskTaken / GameManager.Instance.RiskTotal * 100;
-        }
-        else
-        {
-            riskTakenText.text = "Risk Taken: 0%";
-        }
     }
 }
